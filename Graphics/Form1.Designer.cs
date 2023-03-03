e71fab9a58d97c93fb6db48cee74bf8a3ef6a0cb
idialnik <125955137+idialnik@users.noreply.github.com>
2023-03-03 14:35:10 +0300

    iliya

---
+++ Graphics/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace KEKESGRAPHICAPPLICATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.MoveFigure = new System.Windows.Forms.Button();
+            this.MovePolygon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(14, 451);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MoveFigure
+            // 
+            this.MoveFigure.Location = new System.Drawing.Point(14, 399);
+            this.MoveFigure.Name = "MoveFigure";
+            this.MoveFigure.Size = new System.Drawing.Size(131, 46);
+            this.MoveFigure.TabIndex = 1;
+            this.MoveFigure.Text = "MoveFigure";
+            this.MoveFigure.UseVisualStyleBackColor = true;
+            this.MoveFigure.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MoveFigure_MouseClick);
+            // 
+            // MovePolygon
+            // 
+            this.MovePolygon.Location = new System.Drawing.Point(14, 341);
+            this.MovePolygon.Name = "MovePolygon";
+            this.MovePolygon.Size = new System.Drawing.Size(131, 52);
+            this.MovePolygon.TabIndex = 2;
+            this.MovePolygon.Text = "MovePolygon";
+            this.MovePolygon.UseVisualStyleBackColor = true;
+            this.MovePolygon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MovePolygon_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 558);
+            this.Controls.Add(this.MovePolygon);
+            this.Controls.Add(this.MoveFigure);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button MoveFigure;
+        private System.Windows.Forms.Button MovePolygon;
+    }
+}
+
---
+++ Graphics/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEKESGRAPHICAPPLICATION
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Pen rotatePen = new Pen(Color.HotPink, 3);
+        bool rotateLeft = false;
+
+        List<Point> polygon = new List<Point>();
+        int polygonStep = 1;
+        bool movePolygon = false;
+        Point polygonPoint;
+        Point polygonNext;
+
+        List<Point> path = new List<Point>();
+        List<Point> localPoints = new List<Point>();
+
+        int index = -1;
+        int POINT_SIZE = 6;
+        int HALF_SIZE = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            graphics = CreateGraphics();
+
+            Timer timer = new Timer();
+            timer.Interval = (50);
+            timer.Tick += new EventHandler(Tick);
+            timer.Start();
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            // invokes Form1_Paint
+            Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            path.Clear();
+            localPoints.Clear();
+            movePolygon = false;
+        }
+
+        private void rotateFigureLeft()
+        {
+            Point r = new Point(0, 0);
+
+            for (int i = 0; i < path.Count; ++i)
+            {
+                r.X += path[i].X;
+                r.Y += path[i].Y;
+            }
+
+            r.X /= path.Count;
+            r.Y /= path.Count;
+
+            int angle = 10;
+            double angleRadian = angle * Math.PI / 180;
+
+            for (int i = 0; i < path.Count; ++i)
+            {
+                float x = (float)((path[i].X - r.X) * Math.Cos(angleRadian) - (path[i].Y - r.Y) * Math.Sin(angleRadian) + r.X);
+                float y = (float)((path[i].X - r.X) * Math.Sin(angleRadian) + (path[i].Y - r.Y) * Math.Cos(angleRadian) + r.Y);
+                path[i] = new Point((int)x, (int)y);
+            }
+        }
+
+        private void rotatePolygonLeft()
+        {
+            Point r = new Point(0, 0);
+
+            for (int i = 0; i < polygon.Count; ++i)
+            {
+                r.X += polygon[i].X;
+                r.Y += polygon[i].Y;
+            }
+
+            r.X /= polygon.Count;
+            r.Y /= polygon.Count;
+
+            polygonStep = polygonStep > 10 ? 1 : polygonStep + 1;
+            int angle = 10 * polygonStep;
+            double angleRadian = angle * Math.PI / 180;
+
+            for (int i = 0; i < polygon.Count; ++i)
+            {
+                float x = (float)((polygon[i].X - r.X) * Math.Cos(angleRadian) - (polygon[i].Y - r.Y) * Math.Sin(angleRadian) + r.X);
+                float y = (float)((polygon[i].X - r.X) * Math.Sin(angleRadian) + (polygon[i].Y - r.Y) * Math.Cos(angleRadian) + r.Y);
+                polygon[i] = new Point((int)x, (int)y);
+            }
+
+        }
+
+        private void movePolygonAlongFigure()
+        {
+            polygonPoint = MovePointTowards(polygonPoint, polygonNext, 5);
+            if (polygonPoint.X >= polygonNext.X && polygonPoint.Y >= polygonNext.Y)
+            {
+                for (int i = 0; i < path.Count; ++i)
+                {
+                    if (polygonNext == path[i])
+                    {
+                        polygonNext = i + 1 < path.Count ? path[i + 1] : path[0];
+                        polygonPoint = path[i];
+                        break;
+                    }
+                }
+            }
+
+            polygon.Clear();
+            polygon.Add(new Point(polygonPoint.X - 20, polygonPoint.Y + 20));
+            polygon.Add(new Point(polygonPoint.X + 20, polygonPoint.Y + 20));
+            polygon.Add(new Point(polygonPoint.X + 20, polygonPoint.Y - 20));
+            polygon.Add(new Point(polygonPoint.X - 20, polygonPoint.Y - 20));
+
+            rotatePolygonLeft();
+
+            graphics.DrawPolygon(new Pen(Color.Blue, 3), polygon.ToArray());
+        }
+    
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            graphics.Clear(Color.White);
+
+            if (rotateLeft && path.Count != 0)
+            {
+                rotateFigureLeft();
+            }
+
+            if(movePolygon)
+            {
+                movePolygonAlongFigure();
+            }
+
+            if (path.Count > 2)
+                graphics.DrawPolygon(rotatePen, path.ToArray());
+
+            path.ForEach(point => graphics.FillRectangle(Brushes.DarkGreen,
+                point.X - HALF_SIZE,
+                point.Y - HALF_SIZE,
+                POINT_SIZE,
+                POINT_SIZE)
+            );
+
+
+            localPoints.ForEach(point =>
+            {
+                if (path.Count == 3 && checkPoint(point))
+                {
+                    graphics.FillRectangle(Brushes.DarkSeaGreen,
+                        point.X - HALF_SIZE, point.Y - HALF_SIZE, POINT_SIZE, POINT_SIZE);
+                }
+                else
+                {
+                    graphics.FillRectangle(Brushes.Red,
+                        point.X - HALF_SIZE, point.Y - HALF_SIZE, POINT_SIZE, POINT_SIZE);
+                }
+            });
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // add new point
+                path.Add(e.Location);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                index = -1;
+                for (int i = 0; i < path.Count; i++)
+                {
+                    if ((e.X > path[i].X - HALF_SIZE * 3) & (e.X < path[i].X + HALF_SIZE * 3) &
+                        (e.Y > path[i].Y - HALF_SIZE * 3) & (e.Y < path[i].Y + HALF_SIZE * 3))
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                Console.WriteLine("Point number: " + index);
+            }
+
+            if (e.Button == MouseButtons.Middle)
+            {
+                localPoints.Add(e.Location);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            index = -1;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (index != -1)
+            {
+                path[index] = new Point(e.X, e.Y);
+            }
+        }
+
+        private Point MovePointTowards(Point a, Point b, double distance)
+        {
+            var vector = new Point(b.X - a.X, b.Y - a.Y);
+            var length = Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            var unitVector = new PointF((float)(vector.X / length), (float)(vector.Y / length));
+            return new Point((int)(a.X + unitVector.X * distance), (int)(a.Y + unitVector.Y * distance));
+        }
+
+        private bool checkPoint(Point point)
+        {
+            Point p1 = path[0];
+            Point p2 = path[1];
+            Point p3 = path[2];
+
+            int v1 = (p1.X - point.X) * (p2.Y - p1.Y) - (p2.X - p1.X) * (p1.Y - point.Y);
+            int v2 = (p2.X - point.X) * (p3.Y - p2.Y) - (p3.X - p2.X) * (p2.Y - point.Y);
+            int v3 = (p3.X - point.X) * (p1.Y - p3.Y) - (p1.X - p3.X) * (p3.Y - point.Y);
+
+            if ((v1 >= 0 && v2 >= 0 && v3 >= 0) || (v1 <= 0 && v2 <= 0 && v3 <= 0))
+                return true;
+
+            return false;
+        }
+
+        private void MoveFigure_MouseClick(object sender, MouseEventArgs e)
+        {
+            rotateLeft = !rotateLeft;
+        }
+
+        private void MovePolygon_MouseClick(object sender, MouseEventArgs e)
+        {
+            movePolygon = !movePolygon;
+            polygonPoint = path[0];
+            polygonNext = path[1];
+        }
+    }
+}
